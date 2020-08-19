0b6cf1d8efaee660973b25e4054c8d2831ff39fd
Cameron McEfee <cm@cameronmcefee.com>
2020-08-19 14:28:21 -0700

    update search using sentry-global-search

--- gatsby/src/components/search/index.js
+++ gatsby/src/components/search/index.js
@@ -1,26 +1,19 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "gatsby";
-import {
-  InstantSearch,
-  Index,
-  Hits,
-  Highlight,
-  Snippet,
-  connectStateResults,
-} from "react-instantsearch-dom";
-import algoliasearch from "algoliasearch/lite";
 
-import Input from "./input";
-import * as hitComps from "./hitComps";
+import Logo from "../logo";
 
-const Results = connectStateResults(
-  ({ searchState: state, searchResults: res, children }) =>
-    res && res.nbHits > 0 ? (
-      children
-    ) : (
-      <div className="list-group-item">No results for '{state.query}'</div>
-    )
-);
+import { SentryGlobalSearch } from "sentry-global-search";
+
+const MAX_HITS = 10;
+
+// Add an empty state, for when nothing has results
+
+const search = new SentryGlobalSearch([
+  "docs",
+  "help-center",
+  "develop",
+  "blog",
+]);
 
 const useClickOutside = (ref, handler, events) => {
   if (!events) events = [`mousedown`, `touchstart`];
@@ -42,49 +35,122 @@ const useClickOutside = (ref, handler, events) => {
   });
 };
 
-export default function Search({ indices, collapse, hitsAsGrid }) {
+export default function Search() {
   const ref = useRef(null);
   const [query, setQuery] = useState(``);
+  const [results, setResults] = useState([]);
   const [focus, setFocus] = useState(false);
-  const searchClient = algoliasearch(
-    process.env.GATSBY_ALGOLIA_APP_ID,
-    process.env.GATSBY_ALGOLIA_SEARCH_KEY
-  );
-
+  const [showOffsiteResults, setShowOffsiteResults] = useState(false);
+  const [loading, setLoading] = useState(true);
   useClickOutside(ref, () => setFocus(false));
 
+  const totalHits = results.reduce((a, x) => a + x.hits.length, 0);
+
   return (
-    <InstantSearch
-      searchClient={searchClient}
-      indexName={indices[0].name}
-      onSearchStateChange={({ query }) => setQuery(query)}
-    >
-      <div ref={ref}>
-        <Input onFocus={() => setFocus(true)} />
-        <div
-          style={{ display: query.length > 0 && focus ? "block" : "none" }}
-          className="hits"
-        >
-          <div className="search-results">
-            {indices.map(({ name, title, hitComp, ...props }) => (
-              <Index key={name} indexName={name}>
-                <Results>
-                  <Hits
-                    hitComponent={hitComps[hitComp](() => setFocus(false))}
-                  />
-                </Results>
-              </Index>
+    <div ref={ref}>
+      <input
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+        className="form-control"
+        onChange={({ target: { value: query } }) => {
+          setQuery(query);
+          search.query(query).then((results) => {
+            if (loading) setLoading(false);
+            setResults(results);
+          });
+        }}
+        value={query}
+        onFocus={(e) => setFocus(true)}
+      />
+
+      {query.length > 0 && focus && (
+        <div className="search-results">
+          {loading && <Logo loading={true} />}
+
+          {!loading &&
+            (totalHits > 0 ? (
+              <>
+                <div className="search-results-scroll-container">
+                  {results.map((result, i) => {
+                    const expand = i === 0 || showOffsiteResults;
+                    const hits = result.hits.slice(0, MAX_HITS);
+
+                    if (!expand) return null;
+
+                    return (
+                      <React.Fragment key={result.site}>
+                        {result.site !== "docs" && (
+                          <h4 className="site-result-heading">
+                            From {result.name}
+                          </h4>
+                        )}
+                        <ul
+                          className={`hit-list ${
+                            result.site === "docs" ? "" : "offsite"
+                          }`}
+                        >
+                          {hits.length > 0 ? (
+                            hits.map((hit) => (
+                              <li key={hit.id} className="hit-item">
+                                <a href={hit.url}>
+                                  {hit.title && (
+                                    <h6>
+                                      <span
+                                        dangerouslySetInnerHTML={{
+                                          __html: hit.title,
+                                        }}
+                                      ></span>
+                                    </h6>
+                                  )}
+                                  {hit.text && (
+                                    <span
+                                      dangerouslySetInnerHTML={{
+                                        __html: hit.text,
+                                      }}
+                                    />
+                                  )}
+                                  {hit.context && (
+                                    <div className="hit-context">
+                                      {hit.context.categories &&
+                                        hit.context.categories.length > 0 && (
+                                          <div className="hit-context-left">
+                                            {hit.context.categories.join(", ")}
+                                          </div>
+                                        )}
+                                    </div>
+                                  )}
+                                </a>
+                              </li>
+                            ))
+                          ) : (
+                            <li className="hit-item hit-empty-state">
+                              No results for <em>{query}</em>
+                            </li>
+                          )}
+                        </ul>
+                      </React.Fragment>
+                    );
+                  })}
+                </div>
+                {!showOffsiteResults && (
+                  <div className="expand-results">
+                    <button
+                      className="expand-results-button"
+                      onClick={() => setShowOffsiteResults(true)}
+                    >
+                      Search <em>{query}</em> across all Sentry sites
+                    </button>
+                  </div>
+                )}
+              </>
+            ) : (
+              <div className="hit-empty-state">
+                No results for <em>{query}</em>
+              </div>
             ))}
-            <Index key="legacy" indexName="sentry-docs">
-              <Results>
-                <Hits
-                  hitComponent={hitComps.LegacyPageHit(() => setFocus(false))}
-                />
-              </Results>
-            </Index>
-          </div>
         </div>
-      </div>
-    </InstantSearch>
+      )}
+    </div>
   );
 }
